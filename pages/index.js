78a7263e8f743cf12d996dbd0ca7a72ae8ee146e
Greg Lobinski <greglobinski@gmail.com>
2023-12-12 11:34:52 +0100

    add more forms

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 
 export default function Home() {
+  const forms = Array.apply(null, { length: 30 }).map(Number.call, Number);
+
   return (
     <div className="container">
       <Head>
@@ -10,47 +12,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{ display: `grid`, gap: `1rem` }}>
+      <main style={{ display: `flex`, gap: `1rem` }}>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <form
-          class="form"
-          method="POST"
-          name="contact-1"
-          action="/success"
-          data-netlify="true"
-          style={{ display: `flex`, flexGap: `1rem` }}
-        >
-          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
-          <button>Save</button>
-        </form>
-
-        <form
-          class="form"
-          method="POST"
-          name="contact-2"
-          action="/success"
-          data-netlify="true"
-          style={{ display: `flex`, flexGap: `1rem` }}
-        >
-          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
-          <button>Save</button>
-        </form>
-
-        <form
-          class="form"
-          method="POST"
-          name="contact-3"
-          action="/success"
-          data-netlify="true"
-          style={{ display: `flex`, flexGap: `1rem` }}
-        >
-          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
-          <button>Save</button>
-        </form>
+        {forms.map((item, idx) => (
+          <form
+            class="form"
+            method="POST"
+            name={`form-${idx + 1}`}
+            action="/success"
+            data-netlify="true"
+            style={{ display: `flex`, flexGap: `1rem` }}
+          >
+            <input
+              name="name"
+              style={{ fontSize: `1rem`, padding: `0.5rem` }}
+            />
+            <button>Save</button>
+          </form>
+        ))}
       </main>
 
       <Footer />
