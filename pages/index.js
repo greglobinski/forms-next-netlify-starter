cc21fdbff4717a40ea2d5fe4c21c196de1facf84
Greg Lobinski <greglobinski@gmail.com>
2023-12-12 11:29:40 +0100

    add forms

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -10,14 +10,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main style={{ display: `grid`, gap: `1rem` }}>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+
+        <form
+          class="form"
+          method="POST"
+          name="contact-1"
+          action="/success"
+          data-netlify="true"
+          style={{ display: `flex`, flexGap: `1rem` }}
+        >
+          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
+          <button>Save</button>
+        </form>
+
+        <form
+          class="form"
+          method="POST"
+          name="contact-2"
+          action="/success"
+          data-netlify="true"
+          style={{ display: `flex`, flexGap: `1rem` }}
+        >
+          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
+          <button>Save</button>
+        </form>
+
+        <form
+          class="form"
+          method="POST"
+          name="contact-3"
+          action="/success"
+          data-netlify="true"
+          style={{ display: `flex`, flexGap: `1rem` }}
+        >
+          <input name="name" style={{ fontSize: `1rem`, padding: `0.5rem` }} />
+          <button>Save</button>
+        </form>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
