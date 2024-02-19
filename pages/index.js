cbc522aaca4fe65a22f08e8b7c7979fe91b7414a
Greg Lobinski <greglobinski@gmail.com>
2024-02-19 13:02:25 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -34,62 +34,184 @@ export default function Home() {
         <p>
           Get started by editing <code>pages/index.js</code>
         </p>
-
-        {forms.map((item, idx) => (
-          <form
-            method="POST"
-            data-netlify="true"
-            onSubmit={handleSubmit}
-            name={`ACONTACT`}
+        <form
+          //  action="/fleet-services/commercial-fleet-leasing-finance"
+          name={`A2CONTACT`}
+          data-netlify="true"
+          method="POST"
+          // name="Pardot: Short Form Capabilities Brochure"
+          data-netlify-honeypot="bot-field"
+          className="styles_form_wrapper__AI73A"
+        >
+          <input type="hidden" name="form-name" value="A2CONTACT" />
+          <div className="styles_field_row__r49xc">
+            <div className="styles_field_col__YW0rD">
+              <div className="MuiFormControl-root MuiFormControl-fullWidth css-16ym3lh">
+                <div className="MuiFormControl-root MuiTextField-root css-i44wyl">
+                  <label
+                    className="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined MuiFormLabel-colorPrimary Mui-required MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined css-p0rm37"
+                    data-shrink="false"
+                    for="first_name"
+                    id="first_name-label"
+                  >
+                    First Name
+                    <span
+                      aria-hidden="true"
+                      className="MuiFormLabel-asterisk MuiInputLabel-asterisk css-sp68t1"
+                    ></span>
+                  </label>
+                  <div className="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl css-1v4ccyo">
+                    <input
+                      aria-invalid="false"
+                      autocomplete="first_name"
+                      id="first_name"
+                      name="first_name"
+                      placeholder=""
+                      required=""
+                      type="text"
+                      className="MuiInputBase-input MuiOutlinedInput-input css-1x5jdmq"
+                      value=""
+                    />
+                    <fieldset
+                      aria-hidden="true"
+                      className="MuiOutlinedInput-notchedOutline css-igs3ac"
+                    >
+                      <legend className="css-yjsfm1">
+                        <span>First Name</span>
+                      </legend>
+                    </fieldset>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="styles_field_col__YW0rD">
+              <div className="MuiFormControl-root MuiFormControl-fullWidth css-16ym3lh">
+                <div className="MuiFormControl-root MuiTextField-root css-i44wyl">
+                  <label
+                    className="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined MuiFormLabel-colorPrimary Mui-required MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined css-p0rm37"
+                    data-shrink="false"
+                    for="last_name"
+                    id="last_name-label"
+                  >
+                    Last Name
+                    <span
+                      aria-hidden="true"
+                      className="MuiFormLabel-asterisk MuiInputLabel-asterisk css-sp68t1"
+                    ></span>
+                  </label>
+                  <div className="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl css-1v4ccyo">
+                    <input
+                      aria-invalid="false"
+                      autocomplete="last_name"
+                      id="last_name"
+                      name="last_name"
+                      placeholder=""
+                      required=""
+                      type="text"
+                      className="MuiInputBase-input MuiOutlinedInput-input css-1x5jdmq"
+                      value=""
+                    />
+                    <fieldset
+                      aria-hidden="true"
+                      className="MuiOutlinedInput-notchedOutline css-igs3ac"
+                    >
+                      <legend className="css-yjsfm1">
+                        <span>Last Name</span>
+                      </legend>
+                    </fieldset>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="styles_field_row__r49xc">
+            <div className="styles_field_col__YW0rD">
+              <div className="MuiFormControl-root MuiFormControl-fullWidth css-16ym3lh">
+                <div className="MuiFormControl-root MuiTextField-root css-i44wyl">
+                  <label
+                    className="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined MuiFormLabel-colorPrimary Mui-required MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined css-p0rm37"
+                    data-shrink="false"
+                    for="email"
+                    id="email-label"
+                  >
+                    Email
+                    <span
+                      aria-hidden="true"
+                      className="MuiFormLabel-asterisk MuiInputLabel-asterisk css-sp68t1"
+                    ></span>
+                  </label>
+                  <div className="MuiInputBase-root MuiOutlinedInput-root MuiInputBase-colorPrimary MuiInputBase-formControl css-1v4ccyo">
+                    <input
+                      aria-invalid="false"
+                      autocomplete="email"
+                      id="email"
+                      name="email"
+                      placeholder=""
+                      required=""
+                      type="email"
+                      className="MuiInputBase-input MuiOutlinedInput-input css-1x5jdmq"
+                      value=""
+                    />
+                    <fieldset
+                      aria-hidden="true"
+                      className="MuiOutlinedInput-notchedOutline css-igs3ac"
+                    >
+                      <legend className="css-yjsfm1">
+                        <span>Email</span>
+                      </legend>
+                    </fieldset>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="styles_field_row__r49xc">
+            <div className="styles_field_col__YW0rD">
+              <div className="styles_recaptcha__LLCos">
+                <div>
+                  <div>
+                    <div
+                    // style="width: 304px; height: 78px;"
+                    >
+                      <textarea
+                        id="g-recaptcha-response"
+                        name="g-recaptcha-response"
+                        className="g-recaptcha-response"
+                        // style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="styles_field_col__YW0rD">
+              <button
+                className="styles_button__VMIgL tyles_theme_default__xVJ2v styles_form_submit__UpEMC"
+                type="submit"
+                disabled=""
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+          <div
+          //</form> style="height: 0px; visibility: hidden;"
           >
-            <input type="hidden" name="form-name" value="ACONTACT" />
-            <p>
-              <label>
-                Your Name:{" "}
-                <input
-                  type="text"
-                  name="name"
-                  className="MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined MuiFormLabel-colorPrimary Mui-required MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined css-p0rm37"
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="text" name="email" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message"></textarea>
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-          // <form
-          //   class="form"
-          //   method="POST"
-          //   name={`form-${idx + 1}`}
-          //   action="/success"
-          //   data-netlify="true"
-          //   style={{ display: `flex`, flexGap: `1rem` }}
-          // >
-          //   {idx % 2 === 0 && (
-          //     <input
-          //       type="hidden"
-          //       name="subject"
-          //       value="Sales inquiry from mysitename.netlify.app"
-          //     />
-          //   )}
-
-          //   <input
-          //     name="name"
-          //     style={{ fontSize: `1rem`, padding: `0.5rem` }}
-          //   />
-          //   <button>Save</button>
-          // </form>
-        ))}
+            <label>
+              Don't fill this out if you are human
+              <input name="bot-field" id="bot-field" />
+            </label>
+          </div>
+          <input type="hidden" name="lt_utm_source" />
+          <input type="hidden" name="lt_utm_campaign" />
+          <input type="hidden" name="lt_utm_medium" />
+          <input type="hidden" name="lt_utm_keyword" />
+          <input type="hidden" name="lt_utm_content" />
+          <input type="hidden" name="lt_utm_adname" />
+          <input type="hidden" name="pathname" />
+          <input type="hidden" name="gaid" />
+          <input type="hidden" name="pardot-endpoint" />
+        </form>
       </main>
 
       <Footer />
