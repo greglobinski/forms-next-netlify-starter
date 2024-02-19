8acaabc8787431fd478807c2bdace3f6d687d404
Greg Lobinski <greglobinski@gmail.com>
2024-02-19 11:42:58 +0100

    add success apge

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 
 export default function Home() {
-  const forms = Array.apply(null, { length: 130 }).map(Number.call, Number);
+  const forms = Array.apply(null, { length: 25 }).map(Number.call, Number);
 
   return (
     <div className="container">
@@ -19,28 +19,57 @@ export default function Home() {
         </p>
 
         {forms.map((item, idx) => (
-          <form
-            class="form"
-            method="POST"
-            name={`form-${idx + 1}`}
-            action="/success"
-            data-netlify="true"
-            style={{ display: `flex`, flexGap: `1rem` }}
-          >
-            {idx % 2 === 0 && (
-              <input
-                type="hidden"
-                name="subject"
-                value="Sales inquiry from mysitename.netlify.app"
-              />
-            )}
-
-            <input
-              name="name"
-              style={{ fontSize: `1rem`, padding: `0.5rem` }}
-            />
-            <button>Save</button>
+          <form name={`NEW-form-${idx + 1}`} method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{" "}
+                <select name="role[]">
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
           </form>
+          // <form
+          //   class="form"
+          //   method="POST"
+          //   name={`form-${idx + 1}`}
+          //   action="/success"
+          //   data-netlify="true"
+          //   style={{ display: `flex`, flexGap: `1rem` }}
+          // >
+          //   {idx % 2 === 0 && (
+          //     <input
+          //       type="hidden"
+          //       name="subject"
+          //       value="Sales inquiry from mysitename.netlify.app"
+          //     />
+          //   )}
+
+          //   <input
+          //     name="name"
+          //     style={{ fontSize: `1rem`, padding: `0.5rem` }}
+          //   />
+          //   <button>Save</button>
+          // </form>
         ))}
       </main>
 
